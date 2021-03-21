9f95b13ef18e1508d7ec80daae07b1da07e272cc
CanIDDosU <emilekrauss1141@gmail.com>
2021-03-21 19:00:20 -0400

    LegendCore added /heal and /suicide

--- PluginDev/LegendCore/src/main/java/com/legendsmp/legendcore/LegendCore.java
+++ PluginDev/LegendCore/src/main/java/com/legendsmp/legendcore/LegendCore.java
@@ -2,6 +2,9 @@ package com.legendsmp.legendcore;
 
 import com.legendsmp.legendcore.events.PlayerChat;
 import com.legendsmp.legendcore.events.PlayerJoinQuiz;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LegendCore extends JavaPlugin {
@@ -22,5 +25,35 @@ public final class LegendCore extends JavaPlugin {
         // Notify console that LegendCore has been fully disabled
         getLogger().info("LegendCore DISABLED");
     }
-}
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equals("suicide")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                player.sendMessage("Bye Bye :D");
+                player.setHealth(0);
+
+            } else {
+                System.out.println("You need to be a player to do this command.");
+            }
+
+        }
+
+
+        if (command.getName().equals("heal")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                player.sendMessage("Here you go");
+                player.setHealth(20);
+            } else {
+                System.out.println("You need to be a player to do this command.");
+                return false;
+            }
+
+
+        }
+
+        return false;
+    }
+}
--- PluginDev/LegendCore/src/main/java/com/legendsmp/legendcore/commands/WeatherCommand.java
+++ PluginDev/LegendCore/src/main/java/com/legendsmp/legendcore/commands/WeatherCommand.java
@@ -1,14 +1,57 @@
 package com.legendsmp.legendcore.commands;
 
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.weather.WeatherChangeEvent;
 
 import static org.bukkit.Bukkit.getLogger;
+import static org.bukkit.Bukkit.getWorld;
 
 public class WeatherCommand implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+        if (commandSender instanceof Player) {
+            Player p = (Player) commandSender;
+            getLogger().info("Command [/weather] executed by " + p.getName());
+
+        } else {
+            getLogger().info("Command [/weather] executed");
+        }
+
+        World w = getWorld("world");
+        if (args[0].equals("storm")) {
+            w.setStorm(true);
+            w.setThundering(true);
+
+
+        } else if (args[0].equals("rain")) {
+            w.setStorm(true);
+            w.setThundering(false);
+
+
+        } else if (args[0].equals("clear")) {
+            w.setStorm(false);
+            w.setThundering(false);
+
+
+        } else {
+            commandSender.sendMessage("Error Unknown Weather Arguments");
+
+        }
+
+
+
+
+
+
+
+
         return false;
+
+
+
     }
 }
