36475f804855bb30b2df6e82f52eb8d6ad483551
CanIDDosU <emilekrauss1141@gmail.com>
2021-03-21 17:58:59 -0400

    LegendCore, initial commit of /weather

---
+++ PluginDev/LegendCore/src/main/java/com/legendsmp/legendcore/commands/WeatherCommand.java
@@ -0,0 +1,14 @@
+package com.legendsmp.legendcore.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import static org.bukkit.Bukkit.getLogger;
+
+public class WeatherCommand implements CommandExecutor {
+    @java.lang.Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, java.lang.String[] strings) {
+        return false;
+    }
+}
